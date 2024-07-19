fec7010bff356eb12d2045775366803d43e019d6
NazaCarp <n.l.d.naza@gmail.com>
2024-07-19 14:55:47 -0300

    Especificar versión de Python para Vercel

--- app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const mainButton = document.getElementById('mainButton');
-    mainButton.addEventListener('click', function() {
-        Telegram.WebApp.sendData("Botón principal clicado");
-    });
-
-    Telegram.WebApp.ready();
-    Telegram.WebApp.expand();
-});
