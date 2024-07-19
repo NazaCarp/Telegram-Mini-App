91e0335f1fe7736e811cc0f6089720cfdd9bcaf7
NazaCarp <n.l.d.naza@gmail.com>
2024-07-19 14:22:12 -0300

    Actualizar archivo migration_script.py

---
+++ migration_script.py
@@ -0,0 +1,22 @@
+# migration_script.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, Counter
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Eliminar la tabla existente
+Base.metadata.drop_all(bind=engine, tables=[Counter.__table__])
+
+# Crear la tabla nuevamente
+Base.metadata.create_all(bind=engine)
+
+# Agregar un registro inicial
+db = SessionLocal()
+counter = Counter(value=0)
+db.add(counter)
+db.commit()
+db.close()
