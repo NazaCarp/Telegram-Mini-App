033b3222171958401a9ab1b94410bfbe4cee08e4
NazaCarp <n.l.d.naza@gmail.com>
2024-07-19 11:05:01 -0300

    Actualizar archivo models.py

---
+++ models.py
@@ -0,0 +1,8 @@
+# models.py
+from sqlalchemy import Column, Integer
+from db import Base
+
+class Counter(Base):
+    __tablename__ = "counter"
+    id = Column(Integer, primary_key=True, index=True)
+    value = Column(Integer, default=0)
