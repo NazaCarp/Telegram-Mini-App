9673649a979c3d424d871e1bda24fb30fe5f0064
NazaCarp <n.l.d.naza@gmail.com>
2024-07-20 15:06:03 -0300

    Arreglar el uso de timestamp a Hamster Clone

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def get_counters():
         user_id = int(user_id)  # Convertir a int si es necesario
         counter = db.query(Counter).filter_by(user_id=user_id).first()
         if not counter:
-            counter = Counter(user_id=user_id, score=0, secondarycount=0)
+            counter = Counter(user_id=user_id, score=0, secondarycount=0, timestamp=datetime.utcnow())
             db.add(counter)
             db.commit()
         db.close()
