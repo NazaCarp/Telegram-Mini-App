5dab4a5ec694a382eb4981f3b03439e049ad3dd8
NazaCarp <n.l.d.naza@gmail.com>
2024-07-20 09:30:59 -0300

    Agregar ID

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, BigInteger, Integer
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -6,5 +6,5 @@ Base = declarative_base()
 class Counter(Base):
     __tablename__ = 'counters'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(String, nullable=False, unique=True)
+    user_id = Column(BigInteger, nullable=False, unique=True)
     value = Column(Integer, default=0)
