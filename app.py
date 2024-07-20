dae6ff733020ff0ba96888518bd2e565fa96375e
NazaCarp <n.l.d.naza@gmail.com>
2024-07-20 14:32:09 -0300

    Arreglar el uso de timestamp a Hamster Clone

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ logging.basicConfig(level=logging.DEBUG)
 def home():
     return render_template('index.html')
 
-from datetime import datetime, timezone
-
-from datetime import datetime, timezone
-
 @app.route('/get_counters', methods=['GET'])
 def get_counters():
     user_id = request.args.get('user_id')
