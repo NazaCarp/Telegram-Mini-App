e2a865013768495190d4ec6c358a0c0655ebad25
NazaCarp <n.l.d.naza@gmail.com>
2024-07-31 00:32:51 -0300

    Arreglar el uso del botón de retroceso en la pestaña 'Friends'

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ import logging
 app = Flask(__name__, template_folder='.')
 
 @app.route('/')
+def home():
+    return render_template('.', 'index.html')
+
 @app.route('/index.html')
 def index():
     return render_template('.', 'index.html')
