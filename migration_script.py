accae7616a2e147f416decd2ef2a946e032e4933
NazaCarp <n.l.d.naza@gmail.com>
2024-08-02 03:48:19 -0300

    añadir función de referidos

--- migration_script.py
+++ migration_script.py
@@ -8,8 +8,8 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Eliminar las tablas existentes
-#Base.metadata.drop_all(bind=engine, tables=[Counter.__table__])
-Base.metadata.drop_all(bind=engine)
+Base.metadata.drop_all(bind=engine, tables=[Counter.__table__])
+#Base.metadata.drop_all(bind=engine)
 
 # Crear las tablas nuevamente
 Base.metadata.create_all(bind=engine)
@@ -20,7 +20,7 @@ counter = Counter(user_id=1, name='', score=0, secondarycount=0, tap=1)
 db.add(counter)
 db.commit()
 
-referral = Referral(user_id=1, name='', from_user='', referrals_count=0, referrals_name='', referrals_id=None)
+referral = Referral(user_id=1, name='', from_user='', referrals_count=0, referrals_name='', referrals_id='')
 db.add(referral)
 db.commit()
 
