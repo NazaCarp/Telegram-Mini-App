620dd7197876ebc2983b6b01d3210c2cdf2adf22
NazaCarp <n.l.d.naza@gmail.com>
2024-08-02 10:37:11 -0300

    añadir función de referidos

--- app.py
+++ app.py
@@ -36,20 +36,20 @@ def get_counters():
             db.add(referral)
             db.commit()
         
-        # Incrementar referralscount para el user_id igual a startParam
-        if startParam.isdigit():
-            referrer = db.query(Referral).filter_by(user_id=int(startParam)).first()
-            if referrer:
-                referrer.referrals_count += 1
-                if referrer.referrals_name:
-                    referrer.referrals_name += f', {name}'
-                else:
-                    referrer.referrals_name = name
-                if referrer.referrals_id:
-                    referrer.referrals_id += f', {user_id}'
-                else:
-                    referrer.referrals_id = user_id
-                db.commit()
+            # Incrementar el número de referidos
+            if startParam.isdigit():
+                referrer = db.query(Referral).filter_by(user_id=int(startParam)).first()
+                if referrer:
+                    referrer.referrals_count += 1
+                    if referrer.referrals_name:
+                        referrer.referrals_name += f', {name}'
+                    else:
+                        referrer.referrals_name = name
+                    if referrer.referrals_id:
+                        referrer.referrals_id += f', {user_id}'
+                    else:
+                        referrer.referrals_id = user_id
+                    db.commit()
 
                 referrer_counter = db.query(Counter).filter_by(user_id=int(startParam)).first()
                 referrer_counter.score += 100
