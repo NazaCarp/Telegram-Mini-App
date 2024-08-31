43a447dbd669b055e3a4d1ca0ab7242fba6f4e52
NazaCarp <n.l.d.naza@gmail.com>
2024-08-30 23:17:41 -0300

    implementación del guardado de las tareas completadas

--- migration_script.py
+++ migration_script.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Base, Counter, Referral
+from models import Base, Counter
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
